4b690a3c188983d99b235b6a549f3f9dfb7549d8
Yates <yspearman@redventures.com>
2018-09-24 21:43:25 -0400

    add tests for get /todos

--- server/server.js
+++ server/server.js
@@ -26,8 +26,20 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
-  })
+  });
 });
+
+// app.post('/users', (req, res) => {
+//   var user = new User({
+//     email: req.body.email
+//   });
+//
+//   user.save().then((doc) => {
+//     res.send(doc);
+//   }, (e) => {
+//     res.status(400).send(e);
+//   });
+// });
 
 
 
