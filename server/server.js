88e31b747bb549f68d48fa65cb10b398513b7814
Yates <yspearman@redventures.com>
2018-09-25 21:41:43 -0400

    Add get /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -27,6 +27,33 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+//GET /todos/12341234
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  //validate id using isValid
+    //404 -send back empty body (call send empty)
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+  //findById
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      res.status(404).send();
+    } else {
+    res.send({todo});
+    }
+  }, (e) => {
+    res.status(400).send();
+  });
+    //success
+      //if todo-send back
+      //if no todo -send back 404 with an empty body
+    //error
+      //400 -send nothing
 });
 
 // app.post('/users', (req, res) => {
